db63b7b54a43756c1b4cbf332ca65e8807d5b767
valeriusturcanus <valeratrcn@gmail.com>
2017-10-14 22:48:54 +0100

    first finish

--- ticktac.js
+++ ticktac.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
-    var yourchoice = "", computerchoice = "";
+    var yourchoice = "",
+        computerchoice = "";
     var board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    var winThreat = [[],false], loseJoy = [[],false];
+    var winThreat = [
+            [], false
+        ],
+        loseJoy = [
+            [], false
+        ];
     var gameStatus = "off" //   COULD BE OFF ON WON OR LOST
     var winingCombinations = [
         [0, 1, 2],
@@ -15,12 +21,68 @@ $(document).ready(function() {
     ];
 
     function computermove() {
-        for (var e = 0; e < 9; e++) {
-            if (board[e] == 0) {
-                board[e] = -1
-                // console.log("=computermove=");
+        console.log("COMPUTER MOVE");
+        console.log(loseJoy)
+        console.log(winThreat)
+        threatCheck()
+        console.log(loseJoy);
+        console.log(winThreat);
+        if (loseJoy[1]) {
+            for (e = 0; e < 3; e++) {
+                if (board[loseJoy[0][e]] == 0) {
+                    board[loseJoy[0][e]] = -1;
+                    render();
+                    break;
+                }
+            }
+        } else if (winThreat[1]) {
+            for (e = 0; e < 3; e++) {
                 // console.log(board);
-                break;
+                // console.log();
+                console.log(board[winThreat[0][e]]);
+                if (board[winThreat[0][e]] == 0) {
+                    board[winThreat[0][e]] = -1;
+                    console.log(board);
+                    render();
+                    break;
+                }
+            }
+        } else {
+            var moveSequence = [4, 0, 2, 6, 8, 1, 3, 5, 7]
+            // ---------------------------------------
+            // THIS CODE IS NECESARY TO COMBAT OPOSITE SQUARES FORK WHEN COMPUTER HAS THE MIDDLE
+            // AND THE PLAYER HAS 2 OPOSITE CORNERS
+            var emptySquares = 0;
+            for (var i = 0; i < 9; i++) {
+                if (board[i] == 0) {
+                    emptySquares++
+                }
+            }
+            console.log(emptySquares);
+            if (board[4] == -1 && emptySquares >= 6 &&(board[0]+board[8]==2||board[2]+board[6]==2)) {
+                for (var z = 5; z < 9; z++) {
+                    console.log("weaksquare");
+                    if (board[moveSequence[z]] == 0) {
+                        board[moveSequence[z]] = -1
+                        // console.log("=computermove=");
+                        // console.log(board);
+                        render();
+                        break;
+                    }
+                }
+            }
+            // -----------------------------------------
+            else {
+
+                for (var e = 0; e < 9; e++) {
+                    if (board[moveSequence[e]] == 0) {
+                        board[moveSequence[e]] = -1
+                        // console.log("=computermove=");
+                        // console.log(board);
+                        render();
+                        break;
+                    }
+                }
             }
         }
     }
@@ -38,10 +100,10 @@ $(document).ready(function() {
         }
     }
 
-    function renderWinLine(arraythree){
-        $($("td")[arraythree[0]]).css("background-color","red");
-        $($("td")[arraythree[1]]).css("background-color","red");
-        $($("td")[arraythree[2]]).css("background-color","red");
+    function renderWinLine(arraythree) {
+        $($("td")[arraythree[0]]).css("background-color", "red");
+        $($("td")[arraythree[1]]).css("background-color", "red");
+        $($("td")[arraythree[2]]).css("background-color", "red");
     }
 
     function reset() {
@@ -50,6 +112,12 @@ $(document).ready(function() {
         yourchoice = "";
         board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
         gameStatus = "off";
+        $(".endgame").css("display", "none");
+        winThreat = [
+            [], false
+        ], loseJoy = [
+            [], false
+        ];
     };
 
     function winLostCeck() {
@@ -61,19 +129,59 @@ $(document).ready(function() {
                     total += board[winingCombinations[i][e]]
                 };
                 // console.log(total);
-                if (total == 3){
+                if (total == 3) {
                     gameStatus = "won";
                     renderWinLine(winingCombinations[i]);
+                    $(".endgame").css("display", "block").text("Player : " + gameStatus);
+                    setTimeout(reset, 3000)
                 }
-                if (total == -3){
+                if (total == -3) {
                     gameStatus = "lost";
                     renderWinLine(winingCombinations[i]);
+                    $(".endgame").css("display", "block").text("Player : " + gameStatus);
+                    setTimeout(reset, 2000)
                 }
-
+            }
+            var movesleft = 0;
+            for(var e = 0;e<9;e++){
+                if(board[e]==0){
+                    movesleft++;
+                }
+            }
+            if (movesleft==0){
+                gameStatus = "off";
+                $(".endgame").css("display", "block").text("Player : " + "Tied");
+                setTimeout(reset, 2000)
             }
         }
     }
 
+    function threatCheck() {
+        for (var i = 0; i < 8; i++) {
+            var total = 0;
+            for (var e = 0; e < 3; e++) {
+                // console.log(winingCombinations[i]);
+                total += board[winingCombinations[i][e]]
+            };
+            if (total == 2) {
+                winThreat[0] = winingCombinations[i];
+                winThreat[1] = true;
+                break;
+            }
+            if (total == -2) {
+                loseJoy[0] = winingCombinations[i];
+                loseJoy[1] = true;
+                break;
+            } else {
+                console.log("Assign threats to false");
+                winThreat = [
+                    [], false
+                ], loseJoy = [
+                    [], false
+                ];
+            }
+        }
+    }
     $(".choice").click(function() {
         yourchoice = $(this).text();
         gameStatus = "on";
@@ -82,6 +190,7 @@ $(document).ready(function() {
             computerchoice = "O";
         } else {
             computerchoice = "X";
+            computermove();
         }
     });
 
@@ -89,16 +198,18 @@ $(document).ready(function() {
     $("td").click(function() {
         if (yourchoice.length == 1 && gameStatus == "on") {
             var squareIndex = $(this).attr("class");
-            board[squareIndex] = 1;
-            winLostCeck();
-            if (gameStatus =="on"){
-                computermove();
+            if (board[squareIndex] == 0) {
+                board[squareIndex] = 1;
                 winLostCeck();
+                if (gameStatus == "on") {
+                    computermove();
+                    winLostCeck();
+                }
+                render();
             }
-            render();
         }
     });
-    $("#reset").click(reset);
+    // $("#reset").click(reset);
 
 });
 // NEED TO MAKE AN BOARD ARRAY MAKE THE TIKTAKTOE TO RENDER ACORDING TO THE BOARD
